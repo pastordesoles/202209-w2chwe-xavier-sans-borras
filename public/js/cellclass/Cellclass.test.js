a5b90d995494fb76bf391c74f27a5d660d059168
Xavier Sans <xavi.sans.b@gmail.com>
2022-10-08 16:45:08 +0200

    Fix eslint audit validations

--- public/js/cellclass/Cellclass.test.js
+++ public/js/cellclass/Cellclass.test.js
@@ -3,7 +3,7 @@ import Cell from "./Cellclass.js";
 describe("Given the class Cell", () => {
   describe("When it receives 1, 1 and 1", () => {
     test("Then it should return an object with the properties alive:true, x: 1 and y: 1", () => {
-      const alive = 1;
+      const alive = 0;
       const x = 1;
       const y = 1;
       const expectedCell = new Cell(alive, x, y);
--- public/js/cellgrid/createmap.js
+++ public/js/cellgrid/createmap.js
@@ -4,8 +4,8 @@ import generateRandomNumber from "../cellclass/randomNumber.js";
 const createMap = (rowCount, columnCount) => {
   const map = [];
 
-  for (let x = 0; x < rowCount; x++) {
-    for (let y = 0; y < columnCount; y++) {
+  for (let x = 0; x < rowCount; x + 1) {
+    for (let y = 0; y < columnCount; y + 1) {
       const cell = new Cell(generateRandomNumber(), x, y);
       map[x] = map[x] || [];
       map[x][y] = cell;
